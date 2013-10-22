bcc373f54db3e9b8d1c9785de35e3d16d9bd5f23
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2013-10-22 15:02:05 +0200

    * use correct Inflater / Deflater parameters: we must be RFC 1951 compliant, *not* RFC1950 !! * start work on the AssertionConsumerService

--- src/main/java/nl/knaw/huygens/security/rest/SAMLResource.java
+++ src/main/java/nl/knaw/huygens/security/rest/SAMLResource.java
@@ -1,15 +1,28 @@
 package nl.knaw.huygens.security.rest;
 
+import static com.google.common.base.Charsets.UTF_8;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
 import java.util.UUID;
 import java.util.zip.Deflater;
 import java.util.zip.DeflaterOutputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
+import java.util.zip.InflaterOutputStream;
 
 import org.joda.time.DateTime;
 import org.opensaml.Configuration;
@@ -45,10 +58,32 @@ public class SAMLResource {
     @Path("/auth")
     @Produces(MediaType.TEXT_PLAIN)
     public String getAuthenticationRequest() throws MarshallingException, IOException {
-        final AuthnRequest authnRequest = buildAuthnRequestObject();
-        final String encodedRequestMessage = encodeAuthnRequest(authnRequest);
+        return encodeAuthnRequest(buildAuthnRequestObject());
+    }
 
-        return encodedRequestMessage;
+    @POST
+    @Path("acs")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response assertionConsumerService(@FormParam("SAMLResponse") String base64SamlResponse,
+                                             @FormParam("RelayState") String relayState) {
+        log.debug("assertionConsumerService: RelayState={}, SAMLResponse={}", relayState, base64SamlResponse);
+
+        try {
+            Inflater inflater = new Inflater(true);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            InflaterOutputStream ios = new InflaterOutputStream(baos, inflater);
+            ios.write(Base64.decode(base64SamlResponse));
+            ios.close();
+            final String response = new String(baos.toByteArray());
+            log.debug("decoded response: {}", response);
+        } catch (UnsupportedEncodingException e) {
+            log.warn("Unsupported encoding: {}", UTF_8);
+        } catch (IOException e) {
+            log.warn("IOException: {}", e.getMessage());
+        }
+        // todo: retrieve original URI based on relayState
+        return Response.seeOther(URI.create("/redirect")).build();
     }
 
     private AuthnRequest buildAuthnRequestObject() {
@@ -76,7 +111,8 @@ public class SAMLResource {
         AuthnRequest authnRequest = new AuthnRequestBuilder().buildObject();
         authnRequest.setForceAuthn(false);
         authnRequest.setIsPassive(false);
-        authnRequest.setDestination("http://demo17.huygens.knaw.nl/mujina-idp/SingleSignOnService");
+//        authnRequest.setDestination("http://demo17.huygens.knaw.nl/mujina-idp/SingleSignOnService");
+        authnRequest.setDestination("https://engine.surfconext.nl/authentication/idp/single-sign-on");
         authnRequest.setIssueInstant(new DateTime()); // aka "now"
         authnRequest.setProtocolBinding(SAMLConstants.SAML2_POST_BINDING_URI);
         authnRequest.setAssertionConsumerServiceURL(CONSUMER);
@@ -98,13 +134,41 @@ public class SAMLResource {
         String requestMessage = sw.toString();
         log.debug("requestMessage: {}", requestMessage);
 
-        Deflater deflater = new Deflater(Deflater.DEFLATED, true);
+        Deflater deflater = new Deflater(Deflater.DEFAULT_COMPRESSION, true);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DeflaterOutputStream dos = new DeflaterOutputStream(baos, deflater);
         dos.write(requestMessage.getBytes());
         dos.close();
+        final byte[] source = baos.toByteArray();
+        String base64Message = Base64.encodeBytes(source, Base64.DONT_BREAK_LINES);
+        log.debug("base64Message: {}", base64Message);
 
-        String encodedRequestMessage = Base64.encodeBytes(baos.toByteArray(), Base64.DONT_BREAK_LINES);
+        final byte[] decoded = Base64.decode(base64Message);
+        log.debug("source.len: {}, decoded.len: {}", source.length, decoded.length);
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] != decoded[i]) {
+                log.warn("byte[{}] mismatch", i);
+            }
+        }
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(decoded);
+        Inflater inflater = new Inflater(true);
+        InflaterInputStream iis = new InflaterInputStream(bais, inflater);
+        baos = new ByteArrayOutputStream();
+        byte[] space = new byte[1024];
+        int count = iis.read(space);
+        while (count != -1) {
+            log.debug("Read {} bytes", count);
+            baos.write(space, baos.size(), count);
+            count = iis.read(space);
+        }
+        inflater.end();
+        iis.close();
+
+        log.debug("inflated: {}", baos.toByteArray());
+
+        String encodedRequestMessage = URLEncoder.encode(base64Message, UTF_8.name());
+        log.debug("urlEncodedMessage: {}", encodedRequestMessage);
 
         return encodedRequestMessage;
     }
