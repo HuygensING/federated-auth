6ad5fb43c9f009ad1f368bdb519a7909d3276fb0
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-11-20 11:19:22 +0100

    Mock for AuthenticationHandler added

--- src/main/java/nl/knaw/huygens/security/client/model/SecurityInformation.java
+++ src/main/java/nl/knaw/huygens/security/client/model/SecurityInformation.java
@@ -1,23 +1,26 @@
 package nl.knaw.huygens.security.client.model;
 
 import java.security.Principal;
+import java.util.EnumSet;
+
+import nl.knaw.huygens.security.core.model.HuygensPrincipal.Affiliation;
 
 /**
  * A class that contains the mandatory information, that is needed to create a SecurityContext.
  *
  */
+
+// TODO create interface
 public class SecurityInformation {
-  private String applicationName;
+  private String commonName;
   private String displayName;
+  private String givenName;
+  private String surname;
+  private String emailAddress;
+  private EnumSet<Affiliation> affiliations = EnumSet.noneOf(Affiliation.class);
+  private String organization;
+  private String persistentID;
   private Principal principal;
-
-  public String getApplicationName() {
-    return applicationName;
-  }
-
-  public void setApplicationName(String applicationName) {
-    this.applicationName = applicationName;
-  }
 
   public String getDisplayName() {
     return displayName;
@@ -33,6 +36,62 @@ public class SecurityInformation {
 
   public void setPrincipal(Principal principal) {
     this.principal = principal;
+  }
+
+  public String getCommonName() {
+    return commonName;
+  }
+
+  public void setCommonName(String commonName) {
+    this.commonName = commonName;
+  }
+
+  public String getGivenName() {
+    return givenName;
+  }
+
+  public void setGivenName(String givenName) {
+    this.givenName = givenName;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public void setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
+
+  public EnumSet<Affiliation> getAffiliations() {
+    return affiliations;
+  }
+
+  public void setAffiliations(EnumSet<Affiliation> affiliations) {
+    this.affiliations = affiliations;
+  }
+
+  public String getOrganization() {
+    return organization;
+  }
+
+  public void setOrganization(String organization) {
+    this.organization = organization;
+  }
+
+  public String getPersistentID() {
+    return persistentID;
+  }
+
+  public void setPersistentID(String persistentID) {
+    this.persistentID = persistentID;
   }
 
 }
