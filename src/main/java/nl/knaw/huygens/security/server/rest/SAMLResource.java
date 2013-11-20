9a5af8674a3da95237dd676eea33c625b7d175b0
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2013-11-20 10:16:07 +0100

    Refactored into client, core and server packages

---
+++ src/main/java/nl/knaw/huygens/security/server/rest/SAMLResource.java
@@ -0,0 +1,319 @@
+package nl.knaw.huygens.security.server.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.UUID;
+
+import com.google.common.base.Strings;
+import com.google.inject.Inject;
+import nl.knaw.huygens.security.core.model.HuygensPrincipal;
+import nl.knaw.huygens.security.server.model.HuygensSession;
+import nl.knaw.huygens.security.server.saml2.SAML2PrincipalAttributesMapper;
+import nl.knaw.huygens.security.server.saml2.SAMLEncoder;
+import nl.knaw.huygens.security.server.service.SessionManager;
+import org.joda.time.DateTime;
+import org.opensaml.Configuration;
+import org.opensaml.common.SAMLVersion;
+import org.opensaml.common.xml.SAMLConstants;
+import org.opensaml.saml2.core.Assertion;
+import org.opensaml.saml2.core.Attribute;
+import org.opensaml.saml2.core.AttributeStatement;
+import org.opensaml.saml2.core.AuthnContextClassRef;
+import org.opensaml.saml2.core.AuthnContextComparisonTypeEnumeration;
+import org.opensaml.saml2.core.AuthnRequest;
+import org.opensaml.saml2.core.Issuer;
+import org.opensaml.saml2.core.NameID;
+import org.opensaml.saml2.core.NameIDPolicy;
+import org.opensaml.saml2.core.RequestedAuthnContext;
+import org.opensaml.saml2.core.impl.AuthnContextClassRefBuilder;
+import org.opensaml.saml2.core.impl.AuthnRequestBuilder;
+import org.opensaml.saml2.core.impl.IssuerBuilder;
+import org.opensaml.saml2.core.impl.NameIDPolicyBuilder;
+import org.opensaml.saml2.core.impl.RequestedAuthnContextBuilder;
+import org.opensaml.security.SAMLSignatureProfileValidator;
+import org.opensaml.ws.message.encoder.MessageEncodingException;
+import org.opensaml.xml.XMLObject;
+import org.opensaml.xml.io.Unmarshaller;
+import org.opensaml.xml.io.UnmarshallerFactory;
+import org.opensaml.xml.io.UnmarshallingException;
+import org.opensaml.xml.schema.XSAny;
+import org.opensaml.xml.security.x509.BasicX509Credential;
+import org.opensaml.xml.signature.Signature;
+import org.opensaml.xml.signature.SignatureValidator;
+import org.opensaml.xml.util.Base64;
+import org.opensaml.xml.validation.ValidationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+@Path("/saml2")
+public class SAMLResource {
+    public static final String IDP = "https://engine.surfconext.nl/authentication/idp/single-sign-on";
+
+    private static final Logger log = LoggerFactory.getLogger(SAMLResource.class);
+
+    private final SessionManager sessionManager;
+
+    @Inject
+    public SAMLResource(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @GET
+    @Path("/login")
+    @Produces(MediaType.TEXT_HTML)
+    public Response getAuthenticationRequest() throws MessageEncodingException {
+        final AuthnRequest message = buildAuthnRequestObject();
+        final String defB64Message = SAMLEncoder.deflateAndBase64Encode(message);
+        UriBuilder uriBuilder = UriBuilder.fromPath(IDP);
+        uriBuilder.queryParam("SAMLRequest", defB64Message);
+        uriBuilder.queryParam("RelayState", "0xDeadBeef");
+
+        /* HTTP proxies and the user agent intermediary should not cache SAML protocol messages.
+         * To ensure this, the following rules SHOULD be followed.
+         * When returning SAML protocol messages using HTTP 1.1, HTTP responders SHOULD:
+         * Include a Cache-Control header field set to "no-cache, no-store".
+         * Include a Pragma header field set to "no-cache".
+         */
+        return Response.seeOther(uriBuilder.build()) //
+                .header("Cache-Control", "no-cache, no-store") // See: 3.4.5.1, HTTP and Caching Considerations
+                .header("Pragma", "no-cache")//
+                .build();
+    }
+
+    @POST
+    @Path("acs")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response assertionConsumerService(@FormParam("SAMLResponse") String base64SamlResponse,
+                                             @FormParam("RelayState") String relayState) {
+        log.debug("assertionConsumerService: RelayState={}, SAMLResponse={}", relayState, base64SamlResponse);
+
+        if (Strings.isNullOrEmpty(base64SamlResponse)) {
+            log.warn("Bad request: invalid SAMLResponse parameter");
+            return Response.status(Status.BAD_REQUEST) //
+                    .entity("Missing parameter 'SAMLResponse' (null or empty)") //
+                    .build();
+        }
+
+        if (Strings.isNullOrEmpty(relayState)) {
+            log.warn("Missing (null or empty) RelayState parameter");
+            // let's allow this for now, until we actually need to relate information back to the original request.
+        }
+
+        String samlResponse = new String(Base64.decode(base64SamlResponse));
+        log.debug("decoded samlReponse: {}", samlResponse);
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+
+        SAML2PrincipalAttributesMapper mapper = new SAML2PrincipalAttributesMapper();
+
+        StringBuilder attrs = new StringBuilder();
+        String statusCode = null;
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new ByteArrayInputStream(samlResponse.getBytes()));
+            Element root = doc.getDocumentElement();
+            UnmarshallerFactory uf = Configuration.getUnmarshallerFactory();
+            Unmarshaller unmarshaller = uf.getUnmarshaller(root);
+            XMLObject responseXmlObj = unmarshaller.unmarshall(root);
+            org.opensaml.saml2.core.Response resp = (org.opensaml.saml2.core.Response) responseXmlObj;
+
+            Assertion assertion = resp.getAssertions().get(0);
+            verify(assertion.getSignature());
+
+            String subject = assertion.getSubject().getNameID().getValue();
+            log.debug("subject: {}", subject);
+
+            String issuer = assertion.getIssuer().getValue();
+            log.debug("issuer: {}", issuer);
+
+            for (AttributeStatement attributeStatement : assertion.getAttributeStatements()) {
+                mapper.map(attributeStatement.getAttributes());
+                if (false) {
+                    for (Attribute attribute : attributeStatement.getAttributes()) {
+                        final String name = attribute.getName();
+                        if (name.startsWith("urn:oid")) {
+                            continue; // skip
+                        }
+                        log.debug("attribute.name: {}", name);
+                        for (XMLObject xmlObject : attribute.getAttributeValues()) {
+                            final XSAny xsAny = (XSAny) xmlObject;
+                            final String text = xsAny.getTextContent();
+                            log.debug("+- attribute.value: {}", text);
+                            for (XMLObject child : xsAny.getUnknownXMLObjects()) {
+                                log.debug("   +- child: {}", child);
+                                NameID nameID = (NameID) child;
+                                log.debug("   +- nameID.format: {}", nameID.getFormat());
+                                log.debug("   +- nameID.value: {}", nameID.getValue());
+                            }
+
+                            if ("urn:mace:dir:attribute-def:mail".equals(name)) {
+                                attrs.append("mail: [").append(text).append("]\n");
+                            }
+                            else if ("urn:mace:dir:attribute-def:displayName".equals(name)) {
+                                attrs.append("displayName: [").append(text).append("]\n");
+                            }
+                        }
+                    }
+                }
+            }
+            log.debug("HuygensPrincipal: {}", mapper.getHuygensPrincipal());
+
+            statusCode = resp.getStatus().getStatusCode().getValue();
+            log.debug("statusCode: {}", statusCode);
+        } catch (ParserConfigurationException e) {
+            log.warn("ParserConfigurationException: {}", e.getMessage());
+        } catch (SAXException e) {
+            log.warn("SAXException: {}", e.getMessage());
+        } catch (IOException e) {
+            log.warn("IOException: {}", e.getMessage());
+        } catch (UnmarshallingException e) {
+            log.warn("UnmarshallingException: {}", e.getMessage());
+        }
+
+        final HuygensPrincipal huygensPrincipal = mapper.getHuygensPrincipal();
+        if ("urn:oasis:names:tc:SAML:2.0:status:Success".equals(statusCode)) {
+            log.debug("Authentication successful: adding session");
+            HuygensSession session = new HuygensSession();
+            session.setOwner(huygensPrincipal);
+            session.setExpiresOn(new DateTime()); // TODO: add timeout
+            sessionManager.addSession(session);
+        }
+
+        // todo: retrieve original URI based on relayState
+        return Response.ok("Welcome, " + huygensPrincipal.getGivenName() + "!\n").build();
+    }
+
+    private void verify(Signature signature) {
+        log.debug("Verifying signature: {}", signature);
+
+        try {
+            new SAMLSignatureProfileValidator().validate(signature);
+            log.debug("Valid signature profile");
+
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            X509Certificate certificate = (X509Certificate) certificateFactory
+                    .generateCertificate(new FileInputStream(getCertFile()));
+            PublicKey x509PublicKey = certificate.getPublicKey();
+            log.debug("x509PublicKey.algorithm: {}", x509PublicKey.getAlgorithm());
+            log.debug("x509PublicKey.format: {}", x509PublicKey.getFormat());
+
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(x509PublicKey.getEncoded());
+            KeyFactory keyFactory = KeyFactory.getInstance(x509PublicKey.getAlgorithm());
+            PublicKey publicKey = keyFactory.generatePublic(keySpec);
+
+            BasicX509Credential cred = new BasicX509Credential();
+            cred.setPublicKey(publicKey);
+
+            SignatureValidator signatureValidator = new SignatureValidator(cred);
+            signatureValidator.validate(signature);
+            log.debug("Valid signature");
+        } catch (ValidationException e) {
+            log.warn("Signature Validation Exception: {}", e.getMessage());
+        } catch (CertificateException e) {
+            log.warn("CertificateException: {}", e.getMessage());
+        } catch (URISyntaxException e) {
+            log.warn("URISyntaxException: {}", e.getMessage());
+        } catch (FileNotFoundException e) {
+            log.warn("Certificate file not found: {}", e.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            log.debug("No RSA KeyFactory instance to be found: {}", e.getMessage());
+        } catch (InvalidKeySpecException e) {
+            log.debug("Invalid Key Spec: {}", e.getMessage());
+        }
+    }
+
+    private File getCertFile() throws URISyntaxException {
+        final ClassLoader classLoader = getClass().getClassLoader();
+        final URL resource = classLoader.getResource("/certificates/surfconext.cert");
+        log.debug("getCertFile: resource={}", resource);
+        return new File(resource.toURI());
+    }
+
+    /**
+     * HTTP proxies and the user agent intermediary should not cache SAML protocol messages. To ensure this,
+     * the following rules SHOULD be followed.
+     * <p/>
+     * When returning SAML protocol messages using HTTP 1.1, HTTP responders SHOULD:
+     * Include a Cache-Control header field set to "no-cache, no-store".
+     * Include a Pragma header field set to "no-cache".
+     * <p/>
+     * There are no other restrictions on the use of HTTP headers.
+     */
+    private CacheControl getNoCachingControl() {
+        CacheControl cc = new CacheControl();
+        cc.setNoCache(true);
+        cc.setNoStore(true);
+        return cc;
+    }
+
+    private AuthnRequest buildAuthnRequestObject() {
+        // Issuer object
+        Issuer issuer = new IssuerBuilder().buildObject();
+        issuer.setValue("https://secure.huygens.knaw.nl");
+
+        // NameIDPolicy
+        NameIDPolicy nameIDPolicy = new NameIDPolicyBuilder().buildObject();
+        nameIDPolicy.setFormat("urn:oasis:names:tc:SAML:2.0:nameid-format:persistent");
+        nameIDPolicy.setSPNameQualifier("Issuer");
+        nameIDPolicy.setAllowCreate(true);
+
+        // AuthnContextClass
+        AuthnContextClassRef authnContextClassRef = new AuthnContextClassRefBuilder().buildObject();
+        authnContextClassRef
+                .setAuthnContextClassRef("urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport");
+
+        // AuthnContext
+        RequestedAuthnContext requestedAuthnContext = new RequestedAuthnContextBuilder().buildObject();
+        requestedAuthnContext.setComparison(AuthnContextComparisonTypeEnumeration.EXACT);
+        requestedAuthnContext.getAuthnContextClassRefs().add(authnContextClassRef);
+
+        // AuthnRequest
+        AuthnRequest authnRequest = new AuthnRequestBuilder().buildObject();
+        authnRequest.setForceAuthn(false);
+        authnRequest.setIsPassive(false);
+        authnRequest.setDestination("https://engine.surfconext.nl/authentication/idp/single-sign-on");
+        authnRequest.setIssueInstant(new DateTime()); // aka "now"
+        authnRequest.setProtocolBinding(SAMLConstants.SAML2_POST_BINDING_URI);
+//        authnRequest.setAssertionConsumerServiceURL(CONSUMER);
+        authnRequest.setIssuer(issuer);
+        authnRequest.setNameIDPolicy(nameIDPolicy);
+//        authnRequest.setRequestedAuthnContext(requestedAuthnContext);
+        authnRequest.setID(UUID.randomUUID().toString());
+        authnRequest.setVersion(SAMLVersion.VERSION_20);
+
+        return authnRequest;
+    }
+
+}
---
+++ src/main/java/nl/knaw/huygens/security/server/saml2/SAML2PrincipalAttributesMapper.java
@@ -0,0 +1,228 @@
+package nl.knaw.huygens.security.server.saml2;
+
+import java.util.List;
+
+import nl.knaw.huygens.security.core.model.HuygensPrincipal;
+import org.opensaml.saml2.core.Attribute;
+import org.opensaml.saml2.core.NameID;
+import org.opensaml.xml.XMLObject;
+import org.opensaml.xml.schema.XSAny;
+import org.opensaml.xml.schema.XSString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SAML2PrincipalAttributesMapper {
+    public static final String FORMAT_PERSISTENT = "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent";
+
+    public static final String URN_MACE_CN = "urn:mace:dir:attribute-def:cn";
+
+    public static final String URN_OID_CN = "urn:oid:2.5.4.3";
+
+    public static final String URN_MACE_DISPLAY_NAME = "urn:mace:dir:attribute-def:displayName";
+
+    public static final String URN_OID_DISPLAY_NAME = "urn:oid:2.16.840.1.113730.3.1.241";
+
+    public static final String URN_MACE_GIVEN_NAME = "urn:mace:dir:attribute-def:givenName";
+
+    public static final String URN_OID_GIVEN_NAME = "urn:oid:2.5.4.42";
+
+    public static final String URN_MACE_SURNAME = "urn:mace:dir:attribute-def:sn";
+
+    public static final String URN_OID_SURNAME = "urn:oid:2.5.4.4";
+
+    public static final String URN_MACE_EMAIL = "urn:mace:dir:attribute-def:mail";
+
+    public static final String URN_OID_EMAIL = "urn:oid:0.9.2342.19200300.100.1.3";
+
+    public static final String URN_MACE_AFFILIATION = "urn:mace:dir:attribute-def:eduPersonAffiliation";
+
+    public static final String URN_OID_AFFILIATION = "urn:oid:1.3.6.1.4.1.5923.1.1.1.1";
+
+    public static final String URN_MACE_ORGANIZATION = "urn:mace:terena.org:attribute-def:schacHomeOrganization";
+
+    public static final String URN_OID_ORGANIZATION = "urn:oid:1.3.6.1.4.1.25178.1.2.9";
+
+    public static final String URN_MACE_PERSISTENT_ID = "urn:mace:dir:attribute-def:eduPersonTargetedID";
+
+    public static final String URN_OID_PERSISTENT_ID = "urn:oid:1.3.6.1.4.1.5923.1.1.1.10";
+
+    // In the past, SURFconext used to send the home organisation in the attribute
+    // urn:oid:1.3.6.1.4.1.1466.115.121.1.15, which was incorrect.  Since 2013, the correct oid
+    // urn:oid:1.3.6.1.4.1.25178.1.2.9 is in use.  For reasons of compatibility, the old (wrong)
+    // key is also still sent. It should not be used in new implementations.
+    public static final String URN_OID_LEGACY_ORGA = "urn:oid:1.3.6.1.4.1.1466.115.121.1.15";
+
+    private static final Logger log = LoggerFactory.getLogger(SAML2PrincipalAttributesMapper.class);
+
+    private final HuygensPrincipal huygensPrincipal = new HuygensPrincipal();
+
+    public HuygensPrincipal map(List<Attribute> attributes) {
+        for (Attribute attribute : attributes) {
+            final String name = attribute.getName();
+
+            if (name.startsWith("urn:mace")) {
+                mapMACEAttribute(attribute);
+            }
+            else if (name.startsWith("urn:oid")) {
+                mapOIDAttribute(attribute);
+            }
+            else {
+                log.warn("Unknown attribute schema (neither urn:mace, nor urn:oid): [{}]", name);
+            }
+        }
+
+        return huygensPrincipal;
+    }
+
+    public HuygensPrincipal getHuygensPrincipal() {
+        return huygensPrincipal;
+    }
+
+    private void mapCommonName(Attribute attribute) {
+        final String commonName = getFirstAttributeValueString(attribute);
+        huygensPrincipal.setCommonName(commonName);
+    }
+
+    private void mapDisplayName(Attribute attribute) {
+        final String displayName = getFirstAttributeValueString(attribute);
+        huygensPrincipal.setDisplayName(displayName);
+    }
+
+    private void mapGivenName(Attribute attribute) {
+        final String givenName = getFirstAttributeValueString(attribute);
+        huygensPrincipal.setGivenName(givenName);
+    }
+
+    private void mapSurname(Attribute attribute) {
+        final String surname = getFirstAttributeValueString(attribute);
+        huygensPrincipal.setSurname(surname);
+    }
+
+    private void mapEmailAddress(Attribute attribute) {
+        final String emailAddress = getFirstAttributeValueString(attribute);
+        huygensPrincipal.setEmailAddress(emailAddress);
+    }
+
+    private void mapAffiliation(Attribute attribute) {
+        log.debug("affiliations for [{}]:", attribute.getName());
+        for (XMLObject value : attribute.getAttributeValues()) {
+            final String affiliation = ((XSAny) value).getTextContent();
+            log.debug("  +- [{}]", affiliation);
+            huygensPrincipal.addAffiliation(affiliation);
+        }
+    }
+
+    private void mapOrganization(Attribute attribute) {
+        final String organization = getFirstAttributeValueString(attribute);
+        huygensPrincipal.setOrganization(organization);
+    }
+
+    private void mapPersistentID(Attribute attribute) {
+        final XSAny xsAny = (XSAny) getFirstAttributeValue(attribute);
+        for (XMLObject child : xsAny.getUnknownXMLObjects()) {
+            NameID nameID = (NameID) child;
+            final String format = nameID.getFormat();
+            final String value = nameID.getValue();
+            log.debug("  +- nameID.format: {}", format);
+            log.debug("  +- nameID.value: {}", value);
+            if (!FORMAT_PERSISTENT.equals(format)) {
+                log.warn("Incorrect format {} for persistent id, expected: {}", format, FORMAT_PERSISTENT);
+            }
+            else {
+                huygensPrincipal.setPersistentID(value);
+            }
+        }
+    }
+
+    private XMLObject getFirstAttributeValue(Attribute attribute) {
+        final List<XMLObject> attributeValues = attribute.getAttributeValues();
+
+        if (attributeValues.isEmpty()) {
+            log.warn("Empty attribute values list for attribute: {}", attribute.getName());
+            return null;
+        }
+
+        return attributeValues.get(0);
+    }
+
+    private String getFirstAttributeValueString(Attribute attribute) {
+        XMLObject xmlObj = getFirstAttributeValue(attribute);
+
+        String value = null;
+        if (xmlObj instanceof XSString) {
+            value = ((XSString) xmlObj).getValue();
+        }
+        else if (xmlObj instanceof XSAny) {
+            value = ((XSAny) xmlObj).getTextContent();
+        }
+
+        log.debug("first attribute value for [{}] is [{}]", attribute.getName(), value);
+
+        return value;
+    }
+
+    private void mapMACEAttribute(Attribute attribute) {
+        final String name = attribute.getName();
+        if (URN_MACE_CN.equals(name)) {
+            mapCommonName(attribute);
+        }
+        else if (URN_MACE_DISPLAY_NAME.equals(name)) {
+            mapDisplayName(attribute);
+        }
+        else if (URN_MACE_GIVEN_NAME.equals(name)) {
+            mapGivenName(attribute);
+        }
+        else if (URN_MACE_SURNAME.equals(name)) {
+            mapSurname(attribute);
+        }
+        else if (URN_MACE_EMAIL.equals(name)) {
+            mapEmailAddress(attribute);
+        }
+        else if (URN_MACE_AFFILIATION.equals(name)) {
+            mapAffiliation(attribute);
+        }
+        else if (URN_MACE_ORGANIZATION.equals(name)) {
+            mapOrganization(attribute);
+        }
+        else if (URN_MACE_PERSISTENT_ID.equals(name)) {
+            mapPersistentID(attribute);
+        }
+        else {
+            log.warn("Unmapped urn:mace attribute: {}", name);
+        }
+    }
+
+    private void mapOIDAttribute(Attribute attribute) {
+        final String name = attribute.getName();
+        if (URN_OID_CN.equals(name)) {
+            mapCommonName(attribute);
+        }
+        else if (URN_OID_DISPLAY_NAME.equals(name)) {
+            mapDisplayName(attribute);
+        }
+        else if (URN_OID_GIVEN_NAME.equals(name)) {
+            mapGivenName(attribute);
+        }
+        else if (URN_OID_SURNAME.equals(name)) {
+            mapSurname(attribute);
+        }
+        else if (URN_OID_EMAIL.equals(name)) {
+            mapEmailAddress(attribute);
+        }
+        else if (URN_OID_AFFILIATION.equals(name)) {
+            mapAffiliation(attribute);
+        }
+        else if (URN_OID_ORGANIZATION.equals(name)) {
+            mapOrganization(attribute);
+        }
+        else if (URN_OID_PERSISTENT_ID.equals(name)) {
+            mapPersistentID(attribute);
+        }
+        else if (URN_OID_LEGACY_ORGA.equals(name)) {
+            log.info("Ignoring legacy home organization attribute");
+        }
+        else {
+            log.warn("Unmapped urn:oid attribute: {}", name);
+        }
+    }
+}
