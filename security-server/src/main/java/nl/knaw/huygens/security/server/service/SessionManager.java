78caa28a00c3535341ba8c4915419e97a455d585
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2013-11-26 11:14:57 +0100

    Cleaned up some of the Session stuff

--- security-server/src/main/java/nl/knaw/huygens/security/server/service/SessionManager.java
+++ security-server/src/main/java/nl/knaw/huygens/security/server/service/SessionManager.java
@@ -5,10 +5,7 @@ import java.util.UUID;
 
 import com.google.common.collect.Maps;
 import com.google.inject.Singleton;
-
 import nl.knaw.huygens.security.core.model.HuygensSession;
-import nl.knaw.huygens.security.server.model.HuygensSessionImpl;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +13,7 @@ import org.slf4j.LoggerFactory;
 public class SessionManager {
     private static final Logger log = LoggerFactory.getLogger(SessionManager.class);
 
-    private final Map<UUID, HuygensSessionImpl> sessions;
+    private final Map<UUID, HuygensSession> sessions;
 
     public SessionManager() {
         log.debug("SessionManager created");
@@ -28,12 +25,12 @@ public class SessionManager {
         return sessions.get(sessionKey);
     }
 
-    public void addSession(HuygensSessionImpl session) {
+    public void addSession(HuygensSession session) {
         log.debug("Adding session: [{}]", session.getId());
         sessions.put(session.getId(), session);
     }
 
-    public void removeSession(HuygensSessionImpl session) {
+    public void removeSession(HuygensSession session) {
         log.debug("Removing session: [{}]", session.getId());
         sessions.remove(session.getId());
     }
