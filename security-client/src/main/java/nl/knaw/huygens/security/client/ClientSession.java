134e9f59f9545a2b896e3785da9e289e381ec443
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2013-12-03 13:33:33 +0100

    Undo exposure of expiresAt to client side. Added periodic purging of stale sessions and login requests.

--- security-client/src/main/java/nl/knaw/huygens/security/client/ClientSession.java
+++ security-client/src/main/java/nl/knaw/huygens/security/client/ClientSession.java
@@ -4,24 +4,17 @@ import java.util.UUID;
 
 import nl.knaw.huygens.security.core.model.HuygensPrincipal;
 import nl.knaw.huygens.security.core.model.HuygensSession;
-import org.joda.time.DateTime;
 
 public class ClientSession implements HuygensSession {
   private UUID id;
   private HuygensPrincipal owner;
-  private DateTime expiresAt;
 
   @Override
   public HuygensPrincipal getOwner() {
     return owner;
   }
 
-    @Override
-    public DateTime getExpiresAt() {
-        return expiresAt;
-    }
-
-    @Override
+  @Override
   public UUID getId() {
     return id;
   }
