88fdfc8d3380c29ca90d0a0c91e29ba9aac7c037
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2013-12-09 14:52:59 +0100

    Added SessionResource tests and improved SessionResource-SessionService interaction

---
+++ security-server/src/test/java/nl/knaw/huygens/security/server/rest/RESTHelper.java
@@ -0,0 +1,48 @@
+package nl.knaw.huygens.security.server.rest;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+public class RESTHelper {
+    private Class<?> sutClass;
+
+    public RESTHelper(Object sut) {
+        sutClass = sut.getClass();
+    }
+
+    public Method findMethod(final String path, Class annotation) throws NoSuchMethodException {
+        final String pathGlob = path.replaceAll("<.*>", "\\\\{.*\\\\}");
+        final Path classRestAnnotation = sutClass.getAnnotation(Path.class);
+        final String classRestPrefix = classRestAnnotation == null ? "" : classRestAnnotation.value();
+        for (Method m : sutClass.getDeclaredMethods()) {
+            if (m.getAnnotation(annotation) != null) {
+                final Path methodRestAnnotation = m.getAnnotation(Path.class);
+                final String methodRestSuffix = methodRestAnnotation == null ? "" : methodRestAnnotation.value();
+                final String restPath = classRestPrefix + methodRestSuffix;
+                if (restPath.matches(pathGlob)) {
+                    return m;
+                }
+            }
+        }
+
+        throw new NoSuchMethodException("No @" + annotation.getSimpleName() + " method found for REST path: " + path);
+    }
+
+    public List<String> getConsumedMediaTypes(Method m) {
+        return ImmutableList.copyOf(m.getAnnotation(Consumes.class).value());
+    }
+
+    public List<String> getProducedMediaTypes(Method m) {
+        return ImmutableList.copyOf(m.getAnnotation(Produces.class).value());
+    }
+
+    public List<String> getRolesAllowed(Method m) {
+        return ImmutableList.copyOf(m.getAnnotation(RolesAllowed.class).value());
+    }
+}
---
+++ security-server/src/test/java/nl/knaw/huygens/security/server/rest/SessionResourceTest.java
@@ -0,0 +1,163 @@
+package nl.knaw.huygens.security.server.rest;
+
+import static java.util.UUID.fromString;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static nl.knaw.huygens.security.server.Roles.SESSION_MANAGER;
+import static nl.knaw.huygens.security.server.Roles.SESSION_VIEWER;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.Response;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import com.sun.jersey.api.NotFoundException;
+import nl.knaw.huygens.security.server.ResourceGoneException;
+import nl.knaw.huygens.security.server.model.ServerSession;
+import nl.knaw.huygens.security.server.service.SessionService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SessionResourceTest extends BaseTestCase {
+    public static final String ARBITRARY_VALID_UUID = "11111111-1111-1111-1111-111111111111";
+
+    private static final Logger log = LoggerFactory.getLogger(SessionResourceTest.class);
+
+    @Mock
+    private SessionService sessionService;
+
+    @Mock
+    private ServerSession testSession;
+
+    private String testSessionId;
+
+    private RESTHelper restHelper;
+
+    private SessionResource sut;
+
+    @Before
+    public void setUp() throws Exception {
+        testSessionId = ARBITRARY_VALID_UUID;
+        sut = new SessionResource(sessionService);
+        restHelper = new RESTHelper(sut);
+    }
+
+    @Test
+    public void testGetSessionSuccess() throws Exception {
+        when(sessionService.getSession(fromString(testSessionId))).thenReturn(testSession);
+        when(testSession.isCurrent()).thenReturn(true);
+        when(testSession.isDestroyed()).thenReturn(false);
+
+        final Response response = sut.getSession(testSessionId);
+        verify(sessionService).getSession(fromString(testSessionId));
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.getEntity(), is((Object) testSession));
+
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testGetSessionNotFound() throws Exception {
+        sut.getSession(testSessionId);
+    }
+
+    @Test(expected = ResourceGoneException.class)
+    public void testGetSessionExpired() throws Exception {
+        when(sessionService.getSession(fromString(testSessionId))).thenReturn(testSession);
+        when(testSession.isCurrent()).thenReturn(false);
+        when(testSession.isDestroyed()).thenReturn(false);
+
+        sut.getSession(testSessionId);
+    }
+
+    @Test(expected = ResourceGoneException.class)
+    public void testGetSessionPurged() throws Exception {
+        when(sessionService.getSession(fromString(testSessionId))).thenReturn(testSession);
+        when(testSession.isCurrent()).thenReturn(true);
+        when(testSession.isDestroyed()).thenReturn(true);
+
+        sut.getSession(testSessionId);
+    }
+
+    @Test
+    public void testSessionRefresh() throws Exception {
+        when(testSession.isCurrent()).thenReturn(true);
+        when(sessionService.getSession(fromString(testSessionId))).thenReturn(testSession);
+        when(sessionService.refreshSession(testSession)).thenReturn(testSession);
+
+        final Response response = sut.refreshSession(testSessionId);
+        verify(sessionService).refreshSession(testSession);
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.getEntity(), is((Object) testSession));
+    }
+
+    @Test
+    public void testSessionDestroy() throws Exception {
+        when(testSession.isCurrent()).thenReturn(true);
+        when(sessionService.getSession(fromString(testSessionId))).thenReturn(testSession);
+        when(sessionService.destroySession(testSession)).thenReturn(testSession);
+
+        final Response response = sut.destroySession(testSessionId);
+        verify(sessionService).destroySession(testSession);
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.getEntity(), is((Object) testSession));
+    }
+
+    @Test
+    public void testSessionPurge() throws Exception {
+        when(testSession.isCurrent()).thenReturn(false);
+        when(sessionService.getSession(fromString(testSessionId))).thenReturn(testSession);
+        final Collection<ServerSession> purged = Collections.singleton(testSession);
+        when(sessionService.purge()).thenReturn(purged);
+
+        final Response response = sut.purge();
+        verify(sessionService).purge();
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.getEntity(), is((Object) purged));
+    }
+
+    @Test
+    public void testRESTGetSession() throws Exception {
+        final Method m = restHelper.findMethod("/sessions/<id>", GET.class);
+        final List<String> rolesAllowed = restHelper.getRolesAllowed(m);
+
+        assertThat(rolesAllowed, hasSize(2));
+        assertThat(rolesAllowed, containsInAnyOrder(SESSION_VIEWER, SESSION_MANAGER));
+
+        assertThat(restHelper.getProducedMediaTypes(m), contains(APPLICATION_JSON));
+    }
+
+    @Test
+    public void testRESTDeleteSession() throws Exception {
+        final Method m = restHelper.findMethod("/sessions/<id>", DELETE.class);
+        final List<String> rolesAllowed = restHelper.getRolesAllowed(m);
+
+        assertThat(rolesAllowed, hasSize(1));
+        assertThat(rolesAllowed, contains(SESSION_MANAGER));
+
+        assertThat(restHelper.getProducedMediaTypes(m), contains(APPLICATION_JSON));
+    }
+
+    @Test
+    public void testRESTRefreshSession() throws Exception {
+        final Method m = restHelper.findMethod("/sessions/<id>/refresh", POST.class);
+        final List<String> rolesAllowed = restHelper.getRolesAllowed(m);
+
+        assertThat(rolesAllowed, hasSize(1));
+        assertThat(rolesAllowed, contains(SESSION_MANAGER));
+
+        assertThat(restHelper.getProducedMediaTypes(m), contains(APPLICATION_JSON));
+    }
+}
