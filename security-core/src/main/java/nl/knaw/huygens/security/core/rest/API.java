09971aef26dc579e92e16667b94dea41f75c6767
Martijn Maas <martijn.maas@huygens.knaw.nl>
2013-12-13 15:43:21 +0100

    Client refreshes the session each time the session information is requested.

--- security-core/src/main/java/nl/knaw/huygens/security/core/rest/API.java
+++ security-core/src/main/java/nl/knaw/huygens/security/core/rest/API.java
@@ -1,10 +1,16 @@
 package nl.knaw.huygens.security.core.rest;
 
-public interface API {
+public class API {
     public static final String ID_PARAM = "id";
     public static final String SESSION_AUTHENTICATION_URI = "/sessions";
     public static final String SESSION_AUTHENTICATION_PATH = "/{" + ID_PARAM + "}";
 
     public static final String SESSION_ID_HTTP_PARAM = "hsid";
     public static final String REDIRECT_URL_HTTP_PARAM = "hsurl";
+    
+    public static final String REFRESH_PATH = "refresh";
+    
+    private API(){
+      throw new AssertionError("Non-instantiable class");
+    }
 }
