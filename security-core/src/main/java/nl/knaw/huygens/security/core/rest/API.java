87a96fca3edcf86e625047834850f80a8eca1f8b
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2013-12-16 08:21:02 +0100

    Use API.REFRESH_PATH server side

--- security-core/src/main/java/nl/knaw/huygens/security/core/rest/API.java
+++ security-core/src/main/java/nl/knaw/huygens/security/core/rest/API.java
@@ -1,16 +1,15 @@
 package nl.knaw.huygens.security.core.rest;
 
-public class API {
+public final class API {
     public static final String ID_PARAM = "id";
     public static final String SESSION_AUTHENTICATION_URI = "/sessions";
     public static final String SESSION_AUTHENTICATION_PATH = "/{" + ID_PARAM + "}";
+    public static final String REFRESH_PATH = "/refresh";
 
     public static final String SESSION_ID_HTTP_PARAM = "hsid";
     public static final String REDIRECT_URL_HTTP_PARAM = "hsurl";
-    
-    public static final String REFRESH_PATH = "refresh";
-    
-    private API(){
+
+    private API() {
       throw new AssertionError("Non-instantiable class");
     }
 }
