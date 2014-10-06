6582c7cbbde31e92b8a25fa89f1102feb11bf322
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2014-10-06 14:00:53 +0200

    * fix merge conflicts due to jgitflow weirdness

--- security-server/src/main/java/nl/knaw/huygens/security/server/saml2/SAML2PrincipalAttributesMapper.java
+++ security-server/src/main/java/nl/knaw/huygens/security/server/saml2/SAML2PrincipalAttributesMapper.java
@@ -36,114 +36,112 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SAML2PrincipalAttributesMapper {
-    public static final String FORMAT_PERSISTENT = "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent";
+  public static final String FORMAT_PERSISTENT = "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent";
 
-    public static final String URN_MACE_CN = "urn:mace:dir:attribute-def:cn";
+  public static final String URN_MACE_CN = "urn:mace:dir:attribute-def:cn";
 
-    public static final String URN_OID_CN = "urn:oid:2.5.4.3";
+  public static final String URN_OID_CN = "urn:oid:2.5.4.3";
 
-    public static final String URN_MACE_DISPLAY_NAME = "urn:mace:dir:attribute-def:displayName";
+  public static final String URN_MACE_DISPLAY_NAME = "urn:mace:dir:attribute-def:displayName";
 
-    public static final String URN_OID_DISPLAY_NAME = "urn:oid:2.16.840.1.113730.3.1.241";
+  public static final String URN_OID_DISPLAY_NAME = "urn:oid:2.16.840.1.113730.3.1.241";
 
-    public static final String URN_MACE_GIVEN_NAME = "urn:mace:dir:attribute-def:givenName";
+  public static final String URN_MACE_GIVEN_NAME = "urn:mace:dir:attribute-def:givenName";
 
-    public static final String URN_OID_GIVEN_NAME = "urn:oid:2.5.4.42";
+  public static final String URN_OID_GIVEN_NAME = "urn:oid:2.5.4.42";
 
-    public static final String URN_MACE_SURNAME = "urn:mace:dir:attribute-def:sn";
+  public static final String URN_MACE_SURNAME = "urn:mace:dir:attribute-def:sn";
 
-    public static final String URN_OID_SURNAME = "urn:oid:2.5.4.4";
+  public static final String URN_OID_SURNAME = "urn:oid:2.5.4.4";
 
-    public static final String URN_MACE_EMAIL = "urn:mace:dir:attribute-def:mail";
+  public static final String URN_MACE_EMAIL = "urn:mace:dir:attribute-def:mail";
 
-    public static final String URN_OID_EMAIL = "urn:oid:0.9.2342.19200300.100.1.3";
+  public static final String URN_OID_EMAIL = "urn:oid:0.9.2342.19200300.100.1.3";
 
-    public static final String URN_MACE_AFFILIATION = "urn:mace:dir:attribute-def:eduPersonAffiliation";
+  public static final String URN_MACE_AFFILIATION = "urn:mace:dir:attribute-def:eduPersonAffiliation";
 
-    public static final String URN_OID_AFFILIATION = "urn:oid:1.3.6.1.4.1.5923.1.1.1.1";
+  public static final String URN_OID_AFFILIATION = "urn:oid:1.3.6.1.4.1.5923.1.1.1.1";
 
-    public static final String URN_MACE_ORGANIZATION = "urn:mace:terena.org:attribute-def:schacHomeOrganization";
+  public static final String URN_MACE_ORGANIZATION = "urn:mace:terena.org:attribute-def:schacHomeOrganization";
 
-    public static final String URN_OID_ORGANIZATION = "urn:oid:1.3.6.1.4.1.25178.1.2.9";
+  public static final String URN_OID_ORGANIZATION = "urn:oid:1.3.6.1.4.1.25178.1.2.9";
 
-    public static final String URN_MACE_PERSISTENT_ID = "urn:mace:dir:attribute-def:eduPersonTargetedID";
+  public static final String URN_MACE_PERSISTENT_ID = "urn:mace:dir:attribute-def:eduPersonTargetedID";
 
-    public static final String URN_OID_PERSISTENT_ID = "urn:oid:1.3.6.1.4.1.5923.1.1.1.10";
+  public static final String URN_OID_PERSISTENT_ID = "urn:oid:1.3.6.1.4.1.5923.1.1.1.10";
 
-    // In the past, SURFconext used to send the home organisation in the attribute
-    // urn:oid:1.3.6.1.4.1.1466.115.121.1.15, which was incorrect.  Since 2013, the correct oid
-    // urn:oid:1.3.6.1.4.1.25178.1.2.9 is in use.  For reasons of compatibility, the old (wrong)
-    // key is also still sent. It should not be used in new implementations.
-    public static final String URN_OID_LEGACY_ORGA = "urn:oid:1.3.6.1.4.1.1466.115.121.1.15";
+  // In the past, SURFconext used to send the home organisation in the attribute
+  // urn:oid:1.3.6.1.4.1.1466.115.121.1.15, which was incorrect.  Since 2013, the correct oid
+  // urn:oid:1.3.6.1.4.1.25178.1.2.9 is in use.  For reasons of compatibility, the old (wrong)
+  // key is also still sent. It should not be used in new implementations.
+  public static final String URN_OID_LEGACY_ORGA = "urn:oid:1.3.6.1.4.1.1466.115.121.1.15";
 
-    private static final Logger log = LoggerFactory.getLogger(SAML2PrincipalAttributesMapper.class);
+  private static final Logger log = LoggerFactory.getLogger(SAML2PrincipalAttributesMapper.class);
 
-    private final HuygensPrincipal huygensPrincipal;
+  private final HuygensPrincipal huygensPrincipal;
 
-    public SAML2PrincipalAttributesMapper() {
-        this.huygensPrincipal = new HuygensPrincipal();
+  public SAML2PrincipalAttributesMapper() {
+    this.huygensPrincipal = new HuygensPrincipal();
+  }
+
+  public HuygensPrincipal getHuygensPrincipal() {
+    return huygensPrincipal;
+  }
+
+  public SAML2PrincipalAttributesMapper map(Assertion assertion) {
+    for (AttributeStatement attributeStatement : assertion.getAttributeStatements()) {
+      map(attributeStatement);
     }
 
-    public HuygensPrincipal getHuygensPrincipal() {
-        return huygensPrincipal;
+    return this;
+  }
+
+  public SAML2PrincipalAttributesMapper map(AttributeStatement attributeStatement) {
+    for (Attribute attribute : attributeStatement.getAttributes()) {
+      map(attribute);
     }
 
-    public SAML2PrincipalAttributesMapper map(Assertion assertion) {
-        for (AttributeStatement attributeStatement : assertion.getAttributeStatements()) {
-            map(attributeStatement);
-        }
+    return this;
+  }
 
-        return this;
+  public SAML2PrincipalAttributesMapper map(Attribute attribute) {
+    final String name = attribute.getName();
+
+    if (name.startsWith("urn:mace")) {
+      mapMACEAttribute(attribute);
+    } else if (name.startsWith("urn:oid")) {
+      mapOIDAttribute(attribute);
+    } else {
+      log.warn("Unknown attribute schema (neither urn:mace, nor urn:oid): [{}]", name);
     }
 
-    public SAML2PrincipalAttributesMapper map(AttributeStatement attributeStatement) {
-        for (Attribute attribute : attributeStatement.getAttributes()) {
-            map(attribute);
-        }
+    return this;
+  }
 
-        return this;
-    }
+  private void mapCommonName(Attribute attribute) {
+    final String commonName = getFirstAttributeValueString(attribute);
+    huygensPrincipal.setCommonName(commonName);
+  }
 
-    public SAML2PrincipalAttributesMapper map(Attribute attribute) {
-        final String name = attribute.getName();
+  private void mapDisplayName(Attribute attribute) {
+    final String displayName = getFirstAttributeValueString(attribute);
+    huygensPrincipal.setDisplayName(displayName);
+  }
 
-        if (name.startsWith("urn:mace")) {
-            mapMACEAttribute(attribute);
-        }
-        else if (name.startsWith("urn:oid")) {
-            mapOIDAttribute(attribute);
-        }
-        else {
-            log.warn("Unknown attribute schema (neither urn:mace, nor urn:oid): [{}]", name);
-        }
+  private void mapGivenName(Attribute attribute) {
+    final String givenName = getFirstAttributeValueString(attribute);
+    huygensPrincipal.setGivenName(givenName);
+  }
 
-        return this;
-    }
+  private void mapSurname(Attribute attribute) {
+    final String surname = getFirstAttributeValueString(attribute);
+    huygensPrincipal.setSurname(surname);
+  }
 
-    private void mapCommonName(Attribute attribute) {
-        final String commonName = getFirstAttributeValueString(attribute);
-        huygensPrincipal.setCommonName(commonName);
-    }
-
-    private void mapDisplayName(Attribute attribute) {
-        final String displayName = getFirstAttributeValueString(attribute);
-        huygensPrincipal.setDisplayName(displayName);
-    }
-
-    private void mapGivenName(Attribute attribute) {
-        final String givenName = getFirstAttributeValueString(attribute);
-        huygensPrincipal.setGivenName(givenName);
-    }
-
-    private void mapSurname(Attribute attribute) {
-        final String surname = getFirstAttributeValueString(attribute);
-        huygensPrincipal.setSurname(surname);
-    }
-
-    private void mapEmailAddress(Attribute attribute) {
-        final String emailAddress = getFirstAttributeValueString(attribute);
-        huygensPrincipal.setEmailAddress(emailAddress);
-    }
+  private void mapEmailAddress(Attribute attribute) {
+    final String emailAddress = getFirstAttributeValueString(attribute);
+    huygensPrincipal.setEmailAddress(emailAddress);
+  }
 
   private void mapAffiliation(Attribute attribute) {
     for (XMLObject value : attribute.getAttributeValues()) {
